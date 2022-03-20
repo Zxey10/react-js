c4030666d96350f1745c75963cfc1e01df385bb3
Zxey10 <68563478+Zxey10@users.noreply.github.com>
2022-03-20 13:50:06 +0200

    useReducer

--- login-app-2/src/components/Login/Login.js
+++ login-app-2/src/components/Login/Login.js
@@ -1,44 +1,118 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const EMAIL_ACTIONS = {
+  USER_INPUT: 'USER_INPUT',
+  INPUT_BLUR: 'INPUT_BLUR'
+}
+
+const PASSWORD_ACTIONS = {
+  USER_INPUT: 'USER_INPUT',
+  INPUT_BLUR: 'INPUT_BLUR'
+}
+
+const emailReducer = (state,action) => {
+  if(action.type === EMAIL_ACTIONS.USER_INPUT){
+    return {value: action.payload ,isValid: action.payload.includes('@')}
+  }
+  if(action.type === EMAIL_ACTIONS.INPUT_BLUR){
+    return {value: state.value ,isValid: state.value.includes('@')}
+  }
+  return {value: '' ,isValid: false}
+}
+
+const passwordReducer = (state,action) => {
+  switch(action.type){
+    case PASSWORD_ACTIONS.USER_INPUT:
+      return {value: action.payload ,isValid: action.payload.trim().length > 6}
+    case PASSWORD_ACTIONS.INPUT_BLUR:
+      return {value: state.value ,isValid: state.value.trim().length > 6}
+    default:
+      return state;    
+  }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
+
+
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
 
+  const [emailState,dispatchEmail] = useReducer(emailReducer,{
+    value: '',
+    isValid: false
+  })
+
+  const [passwordState,dispatchPassword] = useReducer(passwordReducer,{
+    value: '',
+    isValid: false
+  })
+
+  const {isValid: emailIsValid} = emailState
+  const {isValid: passwordIsValid} = passwordState
+
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes('@') && enteredPassword.trim().length > 6
-    );
-  },[enteredEmail,enteredPassword])
+    const id = setTimeout(() => {
+      console.log('Form');
+      setFormIsValid(emailIsValid && passwordIsValid);
+    },500);
+    return () => {
+      console.log('Cleanup');
+      clearTimeout(id)
+    }
+  },[emailIsValid,passwordIsValid])
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    //setEnteredEmail(event.target.value);
 
+    dispatchEmail({
+      type: 'USER_INPUT',
+      payload: event.target.value
+    })
     
+    // setFormIsValid(
+    //   event.target.value.includes('@') && passwordState.isValid
+    // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
+    //setEnteredPassword(event.target.value);
+
+     dispatchPassword({
+       type: PASSWORD_ACTIONS.USER_INPUT,
+       payload: event.target.value
+     }) 
+
+    // setFormIsValid(
+    //   emailState.isValid && passwordState.isValid
+    // );
+
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({
+      type: 'INPUT_BLUR'
+    })
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    //setPasswordIsValid(enteredPassword.trim().length > 6);
+    dispatchPassword({
+      type: PASSWORD_ACTIONS.INPUT_BLUR
+    })
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -46,28 +120,28 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordState.isValid === false ? classes.invalid : ''
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
--- login-app-2/src/components/MainHeader/Navigation.js
+++ login-app-2/src/components/MainHeader/Navigation.js
@@ -1,28 +1,34 @@
 import React from 'react';
-
+import AuthContext from '../../store/auth-context';
 import classes from './Navigation.module.css';
+
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return  <nav className={classes.nav}>
+        <ul>
+          {props.isLoggedIn && (
+            <li>
+              <a href="/">Users</a>
+            </li>
+          )}
+          {props.isLoggedIn && (
+            <li>
+              <a href="/">Admin</a>
+            </li>
+          )}
+          {props.isLoggedIn && (
+            <li>
+              <button onClick={props.onLogout}>Logout</button>
+            </li>
+          )}
+        </ul>
+      </nav>
+      }}
+     
+    </AuthContext.Consumer>
   );
 };
 
